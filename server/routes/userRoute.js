fdb898d391e8c78d1b9d8cb2a60fda036dabb5cb
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-10 16:44:07 +0530

    Delete server directory

--- server/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { loginAndGenerateUserSig } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/login',loginAndGenerateUserSig)
-
-
-module.exports = router;
